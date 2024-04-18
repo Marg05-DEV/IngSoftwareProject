0189e9b30050b98a917c60de4f94bf90e88c3b30
Marg05-DEV <giacomobiersack@gmail.com>
2024-04-18 10:53:25 +0200

    aggiunta attributo luogoDiNascita nella classe condomino

--- Classes/Condomino.py
+++ Classes/Condomino.py
@@ -1,7 +1,8 @@
 class Condomino:
-    def __init__(self, nome, cognome, residenza, dataDiNascita, codiceFiscale):
+    def __init__(self, nome, cognome, residenza, dataDiNascita, codiceFiscale, luogoDiNascita):
         self.nome = nome
         self.cognome = cognome
         self.residenza = residenza
         self.dataDiNascita = dataDiNascita
         self.codiceFiscale = codiceFiscale
+        self.luogoDiNascita = luogoDiNascita
