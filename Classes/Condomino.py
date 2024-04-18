71e04d1ba0d0aae270052758bbebffd791f06acb
gioca <giorgiocarfagna02@gmail.com>
2024-04-18 10:50:51 +0200

    aggiunta codice fiscale

--- Classes/Condomino.py
+++ Classes/Condomino.py
@@ -1,6 +1,7 @@
 class Condomino:
-    def __init__(self, nome, cognome, residenza, dataDiNascita):
+    def __init__(self, nome, cognome, residenza, dataDiNascita, codiceFiscale):
         self.nome = nome
         self.cognome = cognome
         self.residenza = residenza
         self.dataDiNascita = dataDiNascita
+        self.codiceFiscale = codiceFiscale
