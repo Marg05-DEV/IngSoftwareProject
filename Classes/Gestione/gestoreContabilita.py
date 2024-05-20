40193a78a6a13c689a55feb75a5dbdb620a71c7c
Davide <davideacciarri25@gmail.com>
2024-05-20 19:55:54 +0200

    Ho provato ad implementare visualizzaStatoPatrimoniale e visualizzaSaldoCassa

--- Classes/Gestione/gestoreContabilita.py
+++ Classes/Gestione/gestoreContabilita.py
@@ -27,5 +27,18 @@ class GestoreContabilita:
                 return {saldo_cassa_contanti,
                         saldo_cassa_A_B}
 
-    def visualizzaStatoPatrimoniale(self):
-        pass
+    def visualizzaStatoPatrimonialeSpese(self, immobile):
+        if os.path.isfile("Dati/Spesa.pickle"):
+            with open("Dati/Spesa.pickle", 'rb') as f:
+                spese = pickle.load(f)
+                for spesa in spese.values():
+                    if spesa.pagata != True and spesa.immobile == immobile:
+                        return "Spesa " + spesa.codice + " non pagata dell'immobile selezionato "
+
+    def visualizzaStatoPatrimonialeRate(self):
+        if os.path.isfile("Dati/Rate.pickle"):
+            with open("Dati/Rate.pickle", 'rb') as f:
+                rate = pickle.load(f)
+                for rata in rate.values():
+                    if rata.pagata != True:
+                        return "Rata " + rata.codice + " non versata dell'immobile selezionato "
