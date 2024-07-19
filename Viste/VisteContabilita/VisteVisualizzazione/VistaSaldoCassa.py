3a99f7323f9790a3ab97cde6f3269ef579276331
Davide <davideacciarri25@gmail.com>
2024-07-19 15:44:37 +0200

    inizio Stato patrimoniale

--- Viste/VisteContabilita/VisteVisualizzazione/VistaSaldoCassa.py
+++ Viste/VisteContabilita/VisteVisualizzazione/VistaSaldoCassa.py
@@ -1,5 +1,108 @@
-from PyQt6.QtWidgets import QWidget
+import datetime
+
+from PyQt6.QtCore import QTimer, Qt, QDate
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QLabel, QTableWidgetItem, QSizePolicy, \
+    QHeaderView, QDateEdit
+
+from Classes.Contabilita.rata import Rata
+from Classes.Contabilita.tabellaMillesimale import TabellaMillesimale
+from Classes.Contabilita.tipoSpesa import TipoSpesa
+from Classes.RegistroAnagrafe.condomino import Condomino
+from Classes.RegistroAnagrafe.unitaImmobiliare import UnitaImmobiliare
 
 
 class VistaSaldoCassa(QWidget):
-    pass
+    def __init__(self):
+        super(VistaSaldoCassa, self).__init__()
+        print("busso")
+        main_layout = QVBoxLayout()
+
+        main_layout.addWidget(self.new_label("Saldo cassa del giorno"))
+        print("busso1")
+        self.table_rate = QTableWidget()
+        self.update_table()
+        main_layout.addWidget(self.table_rate)
+
+        self.msg = QLabel("Messaggio")
+        self.msg.setStyleSheet("color: red; font-weight: bold;")
+        self.msg.hide()
+
+        self.timer = QTimer(self)
+        self.timer.setInterval(5000)
+        self.timer.timeout.connect(self.hide_message)
+
+        main_layout.addWidget(self.msg)
+        print("busso2")
+        main_layout.addWidget(self.saldo("Saldo in Contanti", "Contanti"))
+        main_layout.addWidget(self.saldo("Saldo in Assegni Bancari ", "Assegno Bancario"))
+        print("busso3")
+        self.setLayout(main_layout)
+        self.resize(1200, 650)
+        self.setWindowTitle("Proposta Preventivo")
+
+    def update_table(self):
+        print("crazione tabella")
+        rate_pagate = []
+        self.all_rate = Rata.getAllRate()
+        for rate in self.all_rate.values():
+            if rate.pagata:
+                rate_pagate.append(rate.codice)
+        self.table_rate.setRowCount(len(rate_pagate))
+        self.table_rate.setColumnCount(3)
+
+        self.table_rate.setHorizontalHeaderLabels(["Rata", "Importo", "A/B o Contanti"])
+        i = 0
+        for rata in rate_pagate:
+            rata_pagata = Rata.ricercaRataByCodice(rata)
+            item_text = f"Versante:{rata_pagata.versante}\n"
+            item_table = QTableWidgetItem(item_text)
+
+            item_text1 = f"{rata_pagata.importo}"
+            item_table1 = QTableWidgetItem(item_text1)
+
+            item_text2 = f"{rata_pagata.tipoPagamento}"
+            item_table2 = QTableWidgetItem(item_text2)
+
+            item_table.setFlags(Qt.ItemFlag.NoItemFlags)
+            item_table.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.table_rate.setItem(i, 0, item_table)
+            self.table_rate.setItem(i, 1, item_table1)
+            self.table_rate.setItem(i, 2, item_table2)
+            i += 1
+
+        self.table_rate.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.table_rate.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.table_rate.horizontalHeader().setStretchLastSection(True)
+        self.table_rate.verticalHeader().setSectionsClickable(False)
+        self.table_rate.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.table_rate.resizeRowsToContents()
+        self.table_rate.resizeColumnsToContents()
+        header = self.table_rate.verticalHeader()
+        self.table_rate.setMaximumHeight(header.height())
+        print("create tabella")
+
+        return self.table_rate
+    def new_label(self, testo):
+        print("io")
+        print(datetime.date.today())
+        label = QLabel(testo + "   " + str(datetime.date.today()))
+        return label
+    def saldo(self, testo, tipo_pagameto):
+        importo_totale = 0.0
+        for rate in self.all_rate.values():
+            if rate.pagata:
+                if rate.tipoPagamento == tipo_pagameto:
+                    importo_totale += rate.importo
+        label = QLabel(testo + " ..... " + str(importo_totale))
+        return label
+    def callback(self, msg, tab):
+        print(" callback chiamata", msg)
+        self.update_table()
+        if msg:
+            self.msg.setText(msg)
+            self.msg.show()
+            self.timer.start()
+
+    def hide_message(self):
+        self.msg.hide()
+        self.timer.stop()
--- Viste/VisteContabilita/VisteVisualizzazione/VistaStatoPatrimoniale.py
+++ Viste/VisteContabilita/VisteVisualizzazione/VistaStatoPatrimoniale.py
@@ -1,5 +1,153 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import Qt, QStringListModel, QTimer
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QLineEdit, QCompleter, QLabel, QComboBox, QHBoxLayout, \
+    QPushButton, QListView
+
+from Classes.RegistroAnagrafe.immobile import Immobile
 
 
 class VistaStatoPatrimoniale(QWidget):
-    pass
+    def __init__(self):
+        print("class VistaMenuRegistroAnagrafe - __init__ inizio")
+        print(Immobile.getAllImmobili())
+
+        super(VistaStatoPatrimoniale, self).__init__()
+        self.buttons = {}
+        main_layout = QVBoxLayout()
+
+        find_layout = QGridLayout()
+        completer_list = sorted([item.denominazione for item in Immobile.getAllImmobili().values()])
+        print(completer_list)
+        self.searchbar = QLineEdit()
+        self.searchbar.setPlaceholderText("Ricerca Immobile")
+        self.immobili_completer = QCompleter(completer_list)
+        self.immobili_completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        print(self.immobili_completer.completionModel())
+        self.searchbar.setCompleter(self.immobili_completer)
+        self.lbl_search = QLabel("Ricerca l'immobile da selezionare:")
+        self.lbl_searchType = QLabel("Ricerca per:")
+        self.lbl_search.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignBottom)
+        self.lbl_searchType.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignBottom)
+        self.searchType = QComboBox()
+        self.searchType.addItems(["Denominazione", "Sigla", "Codice"])
+        self.searchType.activated.connect(self.sel_tipo_ricerca)
+        self.immobile_selezionato = QLabel("Nessun immobile selezionato")
+
+        find_layout.addWidget(self.lbl_search, 0, 0, 1, 3)
+        find_layout.addWidget(self.lbl_searchType, 0, 3)
+        find_layout.addWidget(self.searchbar, 1, 0, 1, 3)
+        find_layout.addWidget(self.searchType, 1, 3)
+        find_layout.addWidget(QLabel("Stai selezionando: "), 2, 0, 1, 1)
+        find_layout.addWidget(self.immobile_selezionato, 2, 1, 1, 3)
+
+        self.button_layout = QHBoxLayout()
+
+        self.button_layout.addWidget(self.create_button("Seleziona", self.view_stato_patrimoniale))
+
+        self.searchbar.textChanged.connect(self.selectioning)
+
+        main_layout.addLayout(find_layout)
+        main_layout.addLayout(self.button_layout)
+
+        if self.view_stato_patrimoniale():
+            lbl_frase = QLabel("Spese:")
+            lbl_frase.setStyleSheet("font-weight: bold;")
+            lbl_frase.setFixedSize(lbl_frase.sizeHint())
+
+            main_layout.addWidget(lbl_frase)
+            spese_layout = QHBoxLayout()
+            self.list_view_spese = QListView()
+            self.list_view_spese.setAlternatingRowColors(True)
+            spese_layout.addWidget(self.list_view_spese)
+
+            lbl_frase1 = QLabel("Rate:")
+            lbl_frase1.setStyleSheet("font-weight: bold;")
+            main_layout.addWidget(lbl_frase1)
+
+            rate_layout = QHBoxLayout()
+
+            self.list_view_rate = QListView()
+            self.list_view_rate.setAlternatingRowColors(True)
+            rate_layout.addWidget(self.list_view_rate)
+            self.update_list()
+            self.msg = QLabel("")
+            self.msg.setStyleSheet("color: red; font-weight: bold;")
+            self.msg.hide()
+
+            self.timer = QTimer(self)
+            self.timer.setInterval(5000)
+            self.timer.timeout.connect(self.hide_message)
+
+        self.setLayout(main_layout)
+        self.resize(600, 400)
+        self.setWindowTitle("Stato Patrimoniale")
+
+    def create_button(self, testo, action):
+        button = QPushButton(testo)
+        button.setFixedSize(275, 55)
+        button.setCheckable(True)
+        button.clicked.connect(action)
+        self.buttons[testo] = button
+        return button
+
+    def selectioning(self):
+        immobile = None
+
+        if self.searchType.currentIndex() == 0:  # ricerca per denominazione
+            immobile = Immobile.ricercaImmobileByDenominazione(self.searchbar.text())
+            print("imm: ", immobile)
+        elif self.searchType.currentIndex() == 1:  # ricerca per sigla
+            immobile = Immobile.ricercaImmobileBySigla(self.searchbar.text())
+            print("imm: ", immobile)
+        elif self.searchType.currentIndex() == 2:  # ricerca per codice
+            immobile = Immobile.ricercaImmobileByCodice(self.searchbar.text())
+            print("imm: ", immobile)
+
+        if immobile != None:
+            self.immobile_selezionato.setText(f"{immobile.codice} - {immobile.sigla} - {immobile.denominazione}")
+            self.buttons["Seleziona"].setEnabled(True)
+        else:
+            self.immobile_selezionato.setText("Nessun immobile selezionato")
+            self.buttons["Seleziona"].setEnabled(False)
+
+    def sel_tipo_ricerca(self):
+        print("selected index SEARCHING: " + str(self.searchType.currentIndex()) + " -> " + str(
+            self.searchType.currentText()))
+        lista_completamento = []
+        if self.searchType.currentIndex() == 0:  # ricerca per denominazione
+            lista_completamento = sorted([item.denominazione for item in Immobile.getAllImmobili().values()])
+        elif self.searchType.currentIndex() == 1:  # ricerca per sigla
+            lista_completamento = sorted([item.sigla for item in Immobile.getAllImmobili().values()])
+        elif self.searchType.currentIndex() == 2:  # ricerca per codice
+            lista_completamento = sorted([str(item.codice) for item in Immobile.getAllImmobili().values()])
+        self.immobili_completer.setModel(QStringListModel(lista_completamento))
+        self.selectioning()
+
+    def view_stato_patrimoniale(self):
+        search_text = self.searchbar.text()
+        print(f"Testo della barra di ricerca: {search_text}")
+        immobile = 0
+        if search_text:
+            print("sto cercando...")
+            if self.searchType.currentIndex() == 0:  # ricerca per denominazione
+                immobile = Immobile.ricercaImmobileByDenominazione(search_text)
+                print("imm: ", immobile)
+            elif self.searchType.currentIndex() == 1:  # ricerca per sigla
+                immobile = Immobile.ricercaImmobileBySigla(search_text)
+                print("imm: ", immobile)
+            elif self.searchType.currentIndex() == 2:  # ricerca per codice
+                immobile = Immobile.ricercaImmobileByCodice(search_text)
+                print("imm: ", immobile)
+        if immobile != None:
+            return True
+        else:
+            print("no")
+            return None
+
+    def update_list(self):
+
+    def hide_message(self):
+        self.msg.hide()
+        self.timer.stop()
+        if not self.sel_unitaImmobiliare.condomini:
+            self.msg.setText("Non ci sono condomini assegnati all'unità immobiliare")
+            self.msg.show()
