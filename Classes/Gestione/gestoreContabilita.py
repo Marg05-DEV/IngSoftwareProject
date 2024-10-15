8857bc904e6e64e21ad55ce7c2eb3934041f0235
Davide <davideacciarri25@gmail.com>
2024-10-15 11:14:05 +0200

    per controllo metodi statici

--- Classes/Gestione/gestoreContabilita.py
+++ Classes/Gestione/gestoreContabilita.py
@@ -8,18 +8,6 @@ from Classes.RegistroAnagrafe.unitaImmobiliare import UnitaImmobiliare
 
 
 class GestoreContabilita:
-    def visualizzaSaldoCassa(self):
-        saldo_cassa_contanti = 0.0
-        saldo_cassa_A_B = 0.0
-        if os.path.isfile("Dati/Rata.pickle"):
-            with open("Dati/Rate.pickle", 'rb') as f:
-                rate = pickle.load(f)
-                for rata in rate.values():
-                    if rata.tipoPagamento == "Contanti":
-                        saldo_cassa_contanti += rata.importo
-                    if rata.tipoPagamento == "Assegno Bancario":
-                        saldo_cassa_A_B += rata.importo
-                return {saldo_cassa_contanti, saldo_cassa_A_B}
     @staticmethod
     def generaRicevuta(rata):
 
